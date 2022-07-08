6b637b55567cec8b57e0014890300ba1af67a83c
Komputer-P <wlghks0314@gmail.com>
2022-07-09 02:09:20 +0900

    TASK electron version implemented

---
+++ main.js
@@ -0,0 +1,58 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const appUrl = "http://www.tasko.link";
+
+const createWindow = () => {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 1000,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // Load web application by http url
+  win.loadURL(appUrl);
+
+  // Override redirection event on web app
+  // by reloading redirection URL on win
+  win.webContents.on("new-window", (event, url) => {
+    // Exclude auth popup window
+    console.log(url);
+    if(!url.includes('o/oauth2/auth')) {
+      event.preventDefault();
+      win.loadURL(url);
+    }
+  });
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
